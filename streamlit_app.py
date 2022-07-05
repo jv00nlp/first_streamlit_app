4f7c2de6c89323c490a1d5b28785fe363fe62692
jv00nlp <93138902+jv00nlp@users.noreply.github.com>
2022-07-05 14:25:03 +0200

    Update streamlit_app.py
    -importing Python package library: pandas
    -use a pandas function called read_csv  to pull the data into a dataframe, call it my_fruit_list, display it on the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('Title: Testing streamlit & git')
 
@@ -6,4 +10,6 @@ streamlit.header('Header')
 streamlit.text('Text')
 streamlit.text('More Text')
 streamlit.text('Adding Emojis: 🥣 🥗 🐔 🥑🍞')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
